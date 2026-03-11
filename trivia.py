fe5929da730e63b4e8d4742ef7fb492f07ff40a7
Alan Rodriguez <alanfrodriguez16@gmail.com>
2026-03-11 21:23:23 +0100

    se agregaron if para las preguntas

--- trivia.py
+++ trivia.py
@@ -7,3 +7,32 @@ respuesta_4 = 'elefante'
 
 print('Bienvenido esta es una trivia')
 
+pregunta1 = input('que animal tiene 4 patas y una cola?')
+if pregunta1 == respuesta_1:
+    contador += 1
+    print('haz ganado un punto')
+else:
+    print('incorrecto')
+
+pregunta2 = input('que animal puede volar?')
+if pregunta2 == respuesta_2:
+    contador += 1
+    print('haz ganado un punto')
+else:
+    print('incorrecto')
+
+pregunta3 = input('que animal tiene 4 patas y una cola?')
+if pregunta3 == respuesta_3:
+    contador += 1
+    print('haz ganado un punto')
+else:
+    print('incorrecto')
+
+pregunta4 = input('que animal tiene 4 patas y una cola?')
+if pregunta4 == respuesta_4:
+    contador += 1
+    print('haz ganado un punto')
+else:
+    print('incorrecto')
+
+print(f'fin, haz obtenido {contador} puntos')
