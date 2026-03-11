f0e74757900cd520763eb0d65fd712552a80312f
Alan Rodriguez <alanfrodriguez16@gmail.com>
2026-03-11 21:13:24 +0100

    se agregaron variables

--- trivia.py
+++ trivia.py
@@ -1,1 +1,9 @@
 nombre_usuario = input('ingrese su nombre')
+contador = 0
+respuesta_1 = 'perro'
+respuesta_2 = 'pajaro'
+respuesta_3 = 'leon'
+respuesta_4 = 'elefante'
+
+print('Bienvenido esta es una trivia')
+
